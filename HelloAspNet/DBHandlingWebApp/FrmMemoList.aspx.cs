ae1c2c79800d91c054994bc11e355415a54f4334
hyojin-park24 <gygy332@naver.com>
2021-04-13 16:03:03 +0900

    13장 데이터베이스 연결 및 웹사이트 리스트 목록 만들어서 보기

---
+++ HelloAspNet/DBHandlingWebApp/FrmMemoList.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBHandlingWebApp
+{
+    public partial class FrmMemoList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                SqlCommand cmd = new SqlCommand("ListMemo", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrvMemoList.DataSource = ds;
+                GrvMemoList.DataBind();
+            }
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == System.Data.ConnectionState.Closed) conn.Open();
+                SqlCommand cmd = new SqlCommand("SearchMemo", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue(@"SearchField", CboSearch.SelectedValue);
+                cmd.Parameters.AddWithValue(@"SearchQuery", TxtSearch.Text.Replace("--", ""));
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrvMemoList.DataSource = ds;
+                GrvMemoList.DataBind();
+            }
+        }
+    }
+}
