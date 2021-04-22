4aecc5b9f2180c31df3a0ff16bf00eb6909766e7
hyojin-park24 <gygy332@naver.com>
2021-04-22 12:39:18 +0900

    4/22 커밋

--- MVCSolutions/MyPortpolio/Controllers/AccountController.cs
+++ MVCSolutions/MyPortpolio/Controllers/AccountController.cs
@@ -46,7 +46,7 @@ namespace MyPortpolio.Controllers
                 }
             }
 
-            return null;
+            return View("Login");
         }
 
         private Account CheckAccount(string email, string password)
--- MVCSolutions/MyPortpolio/Controllers/BoardController.cs
+++ MVCSolutions/MyPortpolio/Controllers/BoardController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyPortpolio.Data;
 using MyPortpolio.Models;
+using X.PagedList; //페이지 리스트 추가 2021.4.22
 
 namespace MyPortpolio.Controllers
 {
@@ -20,9 +21,13 @@ namespace MyPortpolio.Controllers
         }
 
         // GET: Board
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? page)
         {
-            return View(await _context.Boards.ToListAsync());
+            var pageNumber = page ?? 1; //page값이 널이면 1
+            var pageSize = 10;
+
+            var boards = await _context.Boards.ToPagedListAsync(pageNumber, pageSize);
+            return View(boards);
         }
 
         // GET: Board/Details/5
@@ -39,7 +44,10 @@ namespace MyPortpolio.Controllers
             {
                 return NotFound();
             }
-
+            //ReadCount 증가 시킴 
+            board.ReadCount += 1;
+            _context.Boards.Update(board);
+            _context.SaveChanges(); //Commit !! 
             return View(board);
         }
 
